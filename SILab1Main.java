ca655a876310b2638ddfeb226141ece9b9c9df1e
stefan-lazarevski <lazarevski055@gmail.com>
2024-04-12 15:41:22 +0200

    Final commit

--- SILab1Main.java
+++ SILab1Main.java
@@ -10,7 +10,7 @@ public class SILab1Main {
 
         List<String> list = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             list.add(sc.next());
         }
 	System.out.println(SILab1_2.makeBigStringFrom(list));
